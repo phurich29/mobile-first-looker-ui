e443fae84cc3472e014a505b09ab7122ce88219e
faztyG <faztyguy2000@gmail.com>
2025-08-01 13:34:25 +0700

    fix error

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-41f5f610'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.nnm2v7d7hm8"
+    "revision": "0.mgopmve4mdg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
